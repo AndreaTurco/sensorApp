d826acc88cdf42ff0cffd01b447d495ef66bb95c
Andrea Turco <andrea.turco@spindox.it>
2018-11-17 10:20:18 +0100

    starting to get list af all sensors

--- android/app/src/main/java/com/iot_app_mqtt/allsensorbridge/AllSensorBridgeManager.java
+++ android/app/src/main/java/com/iot_app_mqtt/allsensorbridge/AllSensorBridgeManager.java
@@ -3,6 +3,9 @@
 package com.iot_app_mqtt.allsensorbridge;
 
 import android.view.View;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import java.util.List;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -11,11 +14,18 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class AllSensorBridgeManager extends SimpleViewManager<View> {
     public static final String REACT_CLASS = "AllSensorBridge";
+    private SensorManager mSensorManager;
+    private String sensorToShow;
 
     @Override
     public String getName() {
         // Tell React the name of the module
         // https://facebook.github.io/react-native/docs/native-components-android.html#1-create-the-viewmanager-subclass
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 1; i < mList.size(); i++) {
+            sensorToShow += ("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
         return REACT_CLASS;
     }
 
@@ -27,8 +37,9 @@ public class AllSensorBridgeManager extends SimpleViewManager<View> {
     }
 
     @ReactProp(name = "exampleProp")
-    public void setExampleProp(View view, String prop) {
+    public String setExampleProp(View view, String prop) {
         // Set properties from React onto your native component via a setter method
         // https://facebook.github.io/react-native/docs/native-components-android.html#3-expose-view-property-setters-using-reactprop-or-reactpropgroup-annotation
+        return sensorToShow;
     }
 }
